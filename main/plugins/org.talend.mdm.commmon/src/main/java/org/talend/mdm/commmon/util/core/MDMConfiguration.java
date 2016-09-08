89c4b412eb4701bcf1cecf7a3edcc2e6588e6a18
Ken Kang <zfkang@talend.com>
2016-09-08 12:45:24 +0800

    TMDM-8267: It would be great if we could provide a feature to deactivate or hide the Import and Export button

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/MDMConfiguration.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/MDMConfiguration.java
@@ -38,7 +38,9 @@ public final class MDMConfiguration {
 
     public static final String TECHNICAL_PASSWORD = "technical.password"; //$NON-NLS-1$
 
-    public static final String EXPORT_INPORT_DEFAULT_COUNT = "1000";//$NON-NLS-1$
+    public static final String MAX_EXPORT_COUNT = "1000"; //$NON-NLS-1$
+
+    public static final String MAX_IMPORT_COUNT = "1000"; //$NON-NLS-1$
 
     private static final Logger logger = Logger.getLogger(MDMConfiguration.class);
 
