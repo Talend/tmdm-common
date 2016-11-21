1e20a72c2341d389fa962c2a298deeff02d98f2c
yjli-talend <yjli@talend.com>
2016-11-21 14:13:44 +0800

    TMDM-10164 remove duplicated nodeToString and styleDocument method.

--- main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/annotation/SchematronProcessor.java
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/metadata/annotation/SchematronProcessor.java
@@ -11,25 +11,24 @@
 
 package org.talend.mdm.commmon.metadata.annotation;
 
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 import org.apache.commons.lang.StringEscapeUtils;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.xsd.XSDAnnotation;
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.MetadataRepository;
+import org.talend.mdm.commmon.util.core.XmlUtil;
 import org.w3c.dom.Element;
 
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.StringReader;
-import java.io.StringWriter;
-
 public class SchematronProcessor implements XmlSchemaAnnotationProcessor {
-
-    private static final TransformerFactory transformerFactory = TransformerFactory.newInstance();
 
     @Override
     public void process(MetadataRepository repository, ComplexTypeMetadata type, XSDAnnotation annotation, XmlSchemaAnnotationProcessorState state) {
@@ -40,11 +39,10 @@ public class SchematronProcessor implements XmlSchemaAnnotationProcessor {
                     StringWriter sw = new StringWriter();
                     Transformer transformer;
                     try {
-                        transformer = transformerFactory.newTransformer();
+                        transformer = XmlUtil.generateTransformer(true);
                     } catch (TransformerConfigurationException e) {
                         throw new RuntimeException("Could not create transformer instance.", e);
                     }
-                    transformer.setOutputProperty("omit-xml-declaration", "yes"); //$NON-NLS-1$ //$NON-NLS-2$
                     try {
                         // TODO This is not really efficient but doing it nicely would require to rewrite a StringEscapeUtils.unescapeXml()
                         transformer.transform(new StreamSource(new StringReader(appInfo.getTextContent())), new StreamResult(sw));
---
+++ main/plugins/org.talend.mdm.commmon/src/main/java/org/talend/mdm/commmon/util/core/XmlUtil.java
@@ -0,0 +1,80 @@
+package org.talend.mdm.commmon.util.core;
+
+import java.io.StringWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.io.DocumentResult;
+import org.dom4j.io.DocumentSource;
+import org.w3c.dom.Node;
+
+public class XmlUtil {
+
+    private static final Logger logger = Logger.getLogger(XmlUtil.class);
+
+    private static final TransformerFactory transformerFactory_Xalan = new org.apache.xalan.processor.TransformerFactoryImpl();
+
+    private static final TransformerFactory transformerFactory_Saxon = new net.sf.saxon.TransformerFactoryImpl();
+
+    public static Transformer generateTransformer() throws TransformerConfigurationException {
+        return transformerFactory_Xalan.newTransformer();
+    }
+
+    public static Transformer generateTransformer(boolean isOmitXmlDeclaration) throws TransformerConfigurationException {
+        Transformer transformer = generateTransformer();
+        if (isOmitXmlDeclaration) {
+            transformer.setOutputProperty("omit-xml-declaration", "yes");
+        } else {
+            transformer.setOutputProperty("omit-xml-declaration", "no");
+        }
+        return transformer;
+    }
+
+    public static Transformer generateTransformer(boolean isOmitXmlDeclaration, boolean isIndent)
+            throws TransformerConfigurationException {
+        Transformer transformer = generateTransformer(isOmitXmlDeclaration);
+        if (isIndent) {
+            transformer.setOutputProperty("indent", "yes");
+        } else {
+            transformer.setOutputProperty("indent", "no");
+        }
+        return transformer;
+    }
+
+    public static String nodeToString(Node n, boolean isOmitXmlDeclaration, boolean isIndent) throws TransformerException {
+        StringWriter sw = new StringWriter();
+        Transformer transformer = generateTransformer(isOmitXmlDeclaration, isIndent);
+        transformer.transform(new DOMSource(n), new StreamResult(sw));
+        return sw.toString();
+    }
+
+    public static String nodeToString(Node n) throws TransformerException {
+        return XmlUtil.nodeToString(n, true, true);
+    }
+
+    public static Document styleDocument(Document document, String stylesheet) throws Exception {
+        // load the transformer using JAXP
+        Transformer transformer = transformerFactory_Saxon.newTransformer(new StreamSource(stylesheet));
+
+        // now lets style the given document
+        DocumentSource source = new DocumentSource(document);
+        DocumentResult result = new DocumentResult();
+        transformer.transform(source, result);
+
+        // return the transformed document
+        Document transformedDoc = result.getDocument();
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("The xml file style transformed successfully ");
+        }
+        return transformedDoc;
+    }
+}
